5735fc43af9e6a91c120a2a8345fbe270d2828ea
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-17 16:57:53 +0900

    FIX: Get all users (reason: Promise await)

--- server/src/routes/v1/user.ts
+++ server/src/routes/v1/user.ts
@@ -1,19 +1,26 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { useRecoilSnapshot } from "recoil";
-import { getConnection } from "typeorm";
+import { getConnection, getRepository } from "typeorm";
+import { User } from "../../entity/User";
 import { UserRepository } from "../../repository/UserRepository";
 import setJsonResponser from "../../utils/setJsonResponser";
 
 const router = Router();
 
-router.get("/users", (req: Request, res: Response, next: NextFunction) => {
-  const userRepository = getConnection().getCustomRepository(UserRepository);
+router.get(
+  "/users",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userRepository = getRepository(User);
+    const users = await userRepository.find({
+      select: ["username", "email", "createdAt", "updatedAt"],
+    });
 
-  setJsonResponser(res, {
-    code: 200,
-    message: "모든 유저 조회성공",
-    payload: userRepository.getAllUsers(),
-  });
-});
+    setJsonResponser(res, {
+      code: 200,
+      message: "모든 유저 조회성공",
+      payload: users,
+    });
+  }
+);
 
 export default router;
