d13f82392a826d467799c154757748de382708bd
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-18 14:05:38 +0900

    ADD: Header Component Layout

--- client/components/Common/Header/index.tsx
+++ client/components/Common/Header/index.tsx
@@ -8,6 +8,13 @@ const Header: React.FunctionComponent<HeaderProps> = () => {
     <React.Fragment>
       <header className={styles.header}>
         <div className={styles.logo}>HLOG</div>
+
+        <nav className={styles.menus}>
+          <li className={styles.menu}>메인</li>
+          <li className={styles.menu}>인기 게시글</li>
+          <li className={styles.menu}>최근 게시글</li>
+          <li className={styles.menu}>설정</li>
+        </nav>
       </header>
     </React.Fragment>
   );
