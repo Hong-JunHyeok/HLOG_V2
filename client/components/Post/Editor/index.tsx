0a8f846ae1c6501b587ef747371bd2fa76cdbde5
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-11-30 11:52:33 +0900

    ADD: Post title input container

--- client/components/Post/Editor/index.tsx
+++ client/components/Post/Editor/index.tsx
@@ -7,6 +7,8 @@ import MarkdownIt from "markdown-it";
 import hljs from "highlight.js";
 import "highlight.js/styles/atom-one-dark.css";
 import "github-markdown-css";
+import { useAuthState } from "../../../contexts/AuthContext";
+import DefaultProfile from "../../../assets/svg/default_profile.svg";
 
 const Editor = () => {
   const markdownIt = new MarkdownIt({
@@ -14,23 +16,52 @@ const Editor = () => {
       if (lang && hljs.getLanguage(lang)) {
         try {
           return hljs.highlight(str, { language: lang }).value;
-        } catch (__) {}
+        } catch (error) {
+          console.error(error);
+        }
       }
 
       return "";
     },
   });
 
-  const [code, onChangeCode] = useInput("");
+  const [title, onChangeTitle, setTitle] = useInput("");
+  const [code, onChangeCode, setCode] = useInput("");
   const [editorMode, setEditorMode] = useState<"EDIT" | "PREVIEW">("EDIT");
+  const authState = useAuthState();
 
   useEffect(() => {
     const interval = setInterval(() => {
-      localStorage.setItem("editorContent", code);
+      if (title || code) {
+        const editorData = JSON.stringify({
+          title,
+          code,
+        });
+
+        localStorage.setItem("editorContent", editorData);
+      }
     }, 2000);
 
     return () => clearInterval(interval);
   }, [code]);
+
+  useEffect(() => {
+    const savedCode: {
+      title: string;
+      code: string;
+    } = JSON.parse(localStorage.getItem("editorContent"));
+
+    if (savedCode.title) {
+      setTitle(savedCode.title);
+    }
+    if (savedCode.code) {
+      setCode(savedCode.code);
+    }
+  }, []);
+
+  if (!authState.myInfo) {
+    return null;
+  }
 
   return (
     <React.Fragment>
@@ -73,6 +104,28 @@ const Editor = () => {
               />
             </Then>
           </If>
+        </div>
+
+        <div className={styles.metaContainer}>
+          <img
+            src={authState.myInfo.profileUrl || DefaultProfile}
+            alt=""
+            className={styles.profileImage}
+            draggable={false}
+          />
+
+          <div className={styles.meta}>
+            <div className={styles.titleContainer}>
+              <h1>게시글 제목</h1>
+              <input
+                type="text"
+                className={styles.titleInput}
+                placeholder="이 게시글의 제목을 입력해주세요."
+                value={title}
+                onChange={onChangeTitle}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
