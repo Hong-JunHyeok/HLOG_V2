2d884bc0ca01a304a57afe8a1b26423d5ed3d599
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2021-12-02 23:50:18 +0900

    ADD: Page like button

--- client/components/Post/Like/index.tsx
+++ client/components/Post/Like/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import styles from "./like.module.scss";
+import { FcLikePlaceholder, FcLike } from "react-icons/fc";
+import useToggle from "../../../hooks/useToggle";
 
 const Like: React.FunctionComponent = () => {
+  const [likeToggleState, onChangeToggleState] = useToggle(false);
+
+  const likeNumber = 999;
+
   return (
     <React.Fragment>
-      <div className={styles.container}></div>
+      <div className={styles.container} onClick={onChangeToggleState}>
+        <div className={styles.likeBtn}>
+          {likeToggleState ? <FcLike /> : <FcLikePlaceholder />}
+        </div>
+
+        <span className={styles.likers}>{likeNumber}</span>
+      </div>
     </React.Fragment>
   );
 };
--- client/components/Post/PostView/index.tsx
+++ client/components/Post/PostView/index.tsx
@@ -45,19 +45,23 @@ const PostView: React.FunctionComponent<IPostViewProps> = (props) => {
       <main className={styles.container}>
         <div className={styles.meta}>
           <h1 className={styles.title}>{postTitle}</h1>
+
           <div className={styles.info}>
-            <If condition={updatedAt !== createdAt}>
-              <Then>
-                <span className={styles.date}>
-                  최근 수정됨 : {dateFormatter(updatedAt)}
-                </span>
-              </Then>
-              <Else>
-                <span className={styles.date}>
-                  작성일 : {dateFormatter(createdAt)}
-                </span>
-              </Else>
-            </If>
+            <div className={styles.flex}>
+              <If condition={updatedAt !== createdAt}>
+                <Then>
+                  <span className={styles.date}>
+                    최근 수정됨 : {dateFormatter(updatedAt)}
+                  </span>
+                </Then>
+                <Else>
+                  <span className={styles.date}>
+                    작성일 : {dateFormatter(createdAt)}
+                  </span>
+                </Else>
+              </If>
+              <Like />
+            </div>
             <div className={`${styles.profileInfo}`}>
               <div className={styles.profile}>
                 <img
