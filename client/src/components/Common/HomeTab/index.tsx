a340dcc4eedae8afe712070a1abdbf2de089176b
Hong-JunHyeok <edb1631@dgsw.hs.kr>
2022-05-30 15:06:06 +0900

    Implement nav link style

--- client/src/components/Common/HomeTab/index.tsx
+++ client/src/components/Common/HomeTab/index.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import StyledHomeTab from './StyledHomeTab';
 
-const MenuList: React.FC = () => (
-  <StyledHomeTab.PostMenu>
-    <li>
-      <Link to="/">인기 게시글</Link>
-    </li>
-    <li>
-      <Link to="/recent">최근 게시글</Link>
-    </li>
-  </StyledHomeTab.PostMenu>
-)
+const MenuList: React.FC = () => {
+  const activeStyle = ({ isActive }: { isActive: boolean }) => ({
+    color: isActive ? '#3182f6' : '#4e5968',
+  });
+
+  return (
+    <StyledHomeTab.PostMenu>
+      <li>
+        <NavLink
+          to="/"
+          style={activeStyle}
+        >
+          인기 게시글
+
+        </NavLink>
+      </li>
+      <li>
+        <NavLink
+          to="/recent"
+          style={activeStyle}
+        >
+          최근 게시글
+
+        </NavLink>
+      </li>
+    </StyledHomeTab.PostMenu>
+  );
+};
 
 export default MenuList;
